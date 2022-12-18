0c2fcbe56884cb8bfd6704abaf6bbd6bcd9fbd50
BacingDev <120773064+BacingDev@users.noreply.github.com>
2022-12-18 22:15:24 +0700

    update baru

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,13 +4,64 @@ import { Container, Box } from "@chakra-ui/react";
 export default function Contact() {
   return (
     <LandingLayout>
-      <Container maxW="2xl" bg="blue.600" centerContent>
-        <Box padding="4" bg="blue.400" color="black" maxW="lg">
-          There are many benefits to a joint design and development system. Not
-          only does it bring benefits to the design team, but it also brings
-          benefits to engineering teams. It makes sure that our experiences have
-          a consistent look and feel, not just in our design specs, but in
-          production.
+      <Container maxW="1024px" bg="blue.600" centerContent>
+        <Box padding="4" bg="blue.500" color="white" maxW="lg">
+          Velit pulvinar curabitur iaculis inceptos nec sed placerat curae mi.
+          Lectus ipsum facilisi leo conubia ultricies elit vitae. Ultrices
+          magnis parturient ac per nunc quisque ullamcorper cubilia. Et cursus
+          ad pharetra lectus sollicitudin phasellus ultricies. Augue parturient
+          odio dolor dapibus lectus malesuada. Facilisis vitae elementum platea
+          euismod dui ante class. Sit consequat iaculis magnis donec dignissim
+          lectus cubilia ut semper. Neque facilisi efficitur dictum dui per
+          consectetur class curae velit. Volutpat consequat maximus sed pretium
+          odio interdum tempus sagittis risus dapibus a. Vestibulum laoreet mi
+          ad euismod dui si nisl fusce. Metus consectetuer sed vitae elit
+          ullamcorper tincidunt lorem placerat eros tristique. Nulla porttitor
+          nam interdum sapien metus porta massa consectetuer feugiat ad. Aptent
+          orci enim iaculis porttitor ligula viverra adipiscing parturient elit
+          massa vivamus. Dui pretium congue tincidunt litora inceptos quis
+          tempus. Lorem letius dictumst rhoncus finibus interdum sagittis
+          pulvinar diam. Facilisis pharetra nec sem finibus ligula montes nulla
+          volutpat tellus suspendisse gravida. Magnis vitae facilisi congue
+          ridiculus conubia dictum proin sed justo condimentum auctor. Maecenas
+          dictumst rhoncus diam lobortis ut. Ut aenean aliquet metus vulputate
+          risus rhoncus tellus nec nostra. Efficitur vitae mi egestas auctor mus
+          maecenas non. Vestibulum phasellus nullam dolor feugiat netus nec
+          quisque. Condimentum habitasse mauris sed litora conubia bibendum
+          lacus odio commodo felis. Sem efficitur malesuada suscipit aenean dui.
+          Nisl lorem odio leo tellus elit dictum diam libero nec. Metus torquent
+          dignissim laoreet adipiscing feugiat enim lorem. Adipiscing nec amet
+          parturient ligula tincidunt vitae porttitor. Netus suscipit proin
+          malesuada in montes litora. Phasellus cursus himenaeos ullamcorper
+          aptent molestie a lacus vehicula ante sapien. Consectetuer volutpat
+          porttitor pharetra tempus blandit proin felis. Cubilia mauris nisi vel
+          letius erat at venenatis habitant proin. Nullam odio nunc eu placerat
+          interdum nostra at. Egestas suscipit neque ridiculus quisque
+          parturient sociosqu non. Nostra ut urna libero ante pulvinar sodales
+          aliquam vivamus aliquet curae suspendisse. Diam nibh mus auctor class
+          ornare porttitor hac. Cras enim placerat finibus porttitor praesent
+          per pulvinar mi maecenas. Interdum rutrum tempor massa tempus lectus
+          nunc. Consectetuer mus maximus nam nec cursus natoque mauris vulputate
+          quisque tempus. In lacinia curabitur sed interdum lorem laoreet
+          commodo. Risus suscipit litora curabitur conubia magna. Porta cubilia
+          cursus dolor venenatis proin arcu. Et maecenas consectetuer quis
+          commodo interdum sem adipiscing ridiculus. Viverra ut urna suscipit
+          aenean gravida iaculis luctus facilisis eu nascetur. Neque ut maecenas
+          class nibh lacus primis aenean placerat non taciti netus. Nec etiam
+          rhoncus ultrices netus mi accumsan curae justo molestie habitant.
+          Egestas dolor dui euismod imperdiet hac potenti dapibus rutrum mollis
+          et. Himenaeos conubia sociosqu in etiam fringilla. Hendrerit commodo
+          finibus netus malesuada dignissim in viverra pulvinar magna turpis
+          per. Primis sit sollicitudin lectus malesuada hac ligula himenaeos
+          auctor vehicula fames. Euismod vehicula nisi pharetra nunc imperdiet
+          ipsum morbi nam. Mollis bibendum lacus accumsan letius si libero
+          tincidunt. Velit hac urna fringilla sit fames senectus himenaeos
+          blandit si nibh molestie. Id lobortis laoreet blandit mauris praesent.
+          Neque ex mollis arcu taciti aliquam lobortis pellentesque praesent
+          turpis placerat. Porttitor et pretium odio parturient scelerisque quam
+          fames tincidunt. Dui montes velit pulvinar magna parturient auctor
+          ultricies. Duis nostra venenatis efficitur pretium sapien dictumst.
+          Aliquam elit hac convallis tellus condimentum per habitasse.
         </Box>
       </Container>
     </LandingLayout>
